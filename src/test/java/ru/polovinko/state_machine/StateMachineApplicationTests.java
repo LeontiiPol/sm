29455a2c6b7a0905964bf12bd83cbd06ce5adb29
Leontii Polovinko <half.leon11@gmail.com>
2025-08-26 11:51:13 +0300

    init

---
+++ src/test/java/ru/polovinko/state_machine/StateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.polovinko.state_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
